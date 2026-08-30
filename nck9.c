2a962c10c37e28680d5e65f005dc0ea91007d939
nehakunjir27-star <nehakunjir27@gmail.com>
2026-08-30 14:27:54 +0530

    Add files via upload

---
+++ nck9.c
@@ -0,0 +1,16 @@
+/*
+program(9)->write a c program to demostrate the use of single-line& multi-line comments.
+*/
+
+# include <stdio.h>
+int main()
+{
+	// This is a single - line comment.
+	
+	/*
+	 This is a multi- line comment.
+	 It can span across multiple lines.
+	 */
+	 
+	 return 0;
+}
